a895cafe7077436c5d110c0cee259f5c5ee60f0d
William Cotton <williamcotton@gmail.com>
2016-10-01 02:23:11 -0500

    v2.1.0

--- index.js
+++ index.js
@@ -1,145 +1,114 @@
-module.exports = () => (data) => new Promise((resolve, reject) => {
-  var info, sections, title, author, structure, verses, chorus, bridge, otherSections
+const isCaps = string => string.toUpperCase() === string
 
-  sections = data.replace('\r\n', '\n').replace(/^\s*\n/gm, '\n').split('\n\n')
-  info = sections[0].split(' - ')
-  title = info[0]
-  author = info[1]
-
-  structure = []
-  verses = []
-  otherSections = {}
-
-  const isCaps = string => string.toUpperCase() === string
-
-  const getLyrics = ({section}) => {
-    const lines = section.split('\n')
-    if (lines.length === 1 && isCaps(lines[0])) {
-      return false
-    }
-    const lyrics = []
-    lines.forEach(line => {
-      if (isCaps(line)) {
-        return
-      }
-      lyrics.push(line)
-    })
-    return lyrics
+const getLyrics = ({section}) => {
+  const lines = section.split('\n')
+  if (lines.length === 1 && isCaps(lines[0])) {
+    return false
   }
-
-  const getChords = ({section}) => {
-    const lines = section.split('\n')
-    if (lines.length === 1 && isCaps(lines[0])) {
-      return false
+  const lyrics = []
+  lines.forEach(line => {
+    if (isCaps(line)) {
+      return
     }
-    const chords = []
-    lines.forEach(line => {
-      if (!isCaps(line) || /.*[A-Z]:/.test(line)) {
-        return
-      }
-      chords.push(line)
-    })
-    return chords
+    lyrics.push(line)
+  })
+  return lyrics
+}
+
+const getChords = ({section}) => {
+  const lines = section.split('\n')
+  if (lines.length === 1 && isCaps(lines[0])) {
+    return false
   }
+  const chords = []
+  lines.forEach(line => {
+    if (!isCaps(line) || /.*[A-Z]:/.test(line)) {
+      return
+    }
+    chords.push(line)
+  })
+  return chords
+}
 
-  const getSectionType = ({section, lyrics, chords, verses, chorus, bridge}) => {
-    // (1st, 2nd, 3rd, and all remaining ordering from cascading logic of !chorus, !bridge, verses.length >= 1)
+const getInfo = ({section}) => {
+  if (/.*[A-Z]:/.test(section)) {
+    const [, info] = section.match(/.*[A-Z]:(.*)/)
+    return info
+  }
+}
 
-    // consider the 1st section with chords to be the verse
-    if (verses.length === 0) {
-      if (lyrics && chords.length > 0) {
-        return 'verse'
-      }
+const getSectionType = ({section, lyrics, chords, sections}) => {
+  // (1st, 2nd, 3rd, and all remaining ordering from cascading logic of !chorus, !bridge, verses.length >= 1)
 
-    // consider the 2nd section with chords to be the chorus
-    } else if (verses.length === 1 && lyrics.length > 0 && chords.length > 0 && !chorus) {
-      return 'chorus'
+  // consider the 1st section with chords to be the verse
+  if (!sections.verse) {
+    if (lyrics && chords.length > 0) {
+      return ['verse']
+    }
+  // consider the 2nd section with chords to be the chorus
+  } else if (sections.verse.count === 1 && lyrics.length > 0 && chords.length > 0 && !sections.chorus) {
+    return ['chorus']
+  // consider the 3rd section with chords to be a bridge
+  } else if (sections.verse.count >= 1 && lyrics.length > 0 && chords.length > 0 && sections.chorus && !sections.bridge) {
+    return ['bridge']
+  // consider all remaining sections with lyrics and no chords to be verses
+  } else if (sections.verse.count >= 1 && lyrics.length > 0 && chords.length === 0 && sections.chorus) {
+    return ['verse']
+  } else {
+    // if LABEL: - defines and gives a section a label
+    if (/.*[A-Z]:/.test(section)) {
+      const [, sectionTitle] = section.match(/(.*[A-Z]):/)
+      return [sectionTitle.toLowerCase()]
+    }
 
-    // consider the 3rd section with chords to be a bridge
-    } else if (verses.length >= 1 && lyrics.length > 0 && chords.length > 0 && chorus && !bridge) {
-      return 'bridge'
+    // CHORUS, CHORUS*N
+    if (section.substr(0, 6) === 'CHORUS') {
+      if (section.length === 6) return ['chorus']
+      if (section[6] === '*') {
+        const repeat = section.split('*')[1]
 
-    // consider all remaining sections with lyrics and no chords to be verses
-    } else if (verses.length >= 1 && lyrics.length > 0 && chords.length === 0 && chorus) {
-      return 'verse'
-    } else {
-      // if LABEL: - defines and gives a section a label
-      if (/.*[A-Z]:/.test(section)) {
-        const [, SECTION_TITLE, info] = section.match(/(.*[A-Z]):(.*)/)
-        const sectionTitle = SECTION_TITLE.toLowerCase()
-        otherSections[sectionTitle] = {lyrics, chords, info}
-        return sectionTitle
-      }
-
-      // CHORUS, CHORUS*N
-      if (section.substr(0, 6) === 'CHORUS') {
-        if (section.length === 6) return 'chorus'
-        if (section[6] === '*') {
-          const repeat = section.split('*')[1]
-
-          const sectionTypes = []
-          let i = 0
-          while (i < repeat) {
-            sectionTypes.push('chorus')
-            i++
-          }
-
-          return sectionTypes
+        const sectionTypes = []
+        let i = 0
+        while (i < repeat) {
+          sectionTypes.push('chorus')
+          i++
         }
+
+        return sectionTypes
       }
-
-      // bridge is built in
-      if (section === 'BRIDGE') return 'bridge'
-
-      // TODO: INSTRUMENTAL (BRIDGE, CHORUS*2)
-      if (section.split(' ')[0] === 'INSTRUMENTAL') return 'instrumental'
-
-      // if LABEL matches on previously defined LABEL:
-      if (section.toLowerCase() && otherSections[section.toLowerCase()]) return section.toLowerCase()
-
-      // otherwise it's a verse
-      if (lyrics && chords.length === 0) return 'verse'
     }
-  }
 
-  const getSectionIndex = (sectionType, structureIndex) => {
-    return structure.reduce((count, _sectionType, index) => {
-      if (_sectionType === sectionType && index < structureIndex) count++
-      return count
-    }, 0)
-  }
+    // bridge is built in
+    if (section === 'BRIDGE') return ['bridge']
 
-  sections.forEach((section, structureIndex) => {
+    // TODO: INSTRUMENTAL (BRIDGE, CHORUS*2)
+    if (section.split(' ')[0] === 'INSTRUMENTAL') return ['instrumental']
+
+    // if LABEL matches on previously defined LABEL:
+    if (section.toLowerCase() && sections[section.toLowerCase()]) return [section.toLowerCase()]
+
+    // otherwise it's a verse
+    if (lyrics && chords.length === 0) return ['verse']
+  }
+}
+
+module.exports = () => (data) => new Promise((resolve, reject) => {
+  const rawSections = data.replace('\r\n', '\n').replace(/^\s*\n/gm, '\n').split('\n\n')
+  resolve(rawSections.reduce(({ title, author, structure, sections }, section, structureIndex) => {
     let lyrics = getLyrics({section})
     let chords = getChords({section})
-    let sectionType = getSectionType({section, lyrics, chords, verses, chorus, bridge})
-
-    if (!sectionType) return
-
-    const sectionCount = Array.isArray(sectionType) ? sectionType.length : 0
-
-    if (sectionType === 'verse') {
-      verses.push({lyrics, chords})
-    } else if (sectionType === 'chorus' || (sectionType && sectionType[0] === 'chorus')) {
-      if (!chorus) chorus = {lyrics, chords}
-      lyrics = chorus.lyrics
-      chords = chorus.chords
-    } else if (sectionType === 'bridge') {
-      if (!bridge) bridge = {lyrics, chords}
-      lyrics = bridge.lyrics
-      chords = bridge.chords
-    }
-
-    const sectionIndex = getSectionIndex(sectionType, structureIndex)
-
-    if (sectionCount > 0) {
-      sectionType.forEach(() => {
-        structure = structure.concat({ sectionType: sectionType[0], lyrics, chords, sectionIndex })
-      })
-    } else {
-      structure = structure.concat({ sectionType, lyrics, chords, sectionIndex })
-    }
-  })
-
-  resolve({title, author, verses, chorus, bridge, structure, otherSections})
+    let info = getInfo({section})
+    let sectionTypes = getSectionType({section, lyrics, chords, sections})
+    if (!sectionTypes) return { structure, sections, title, author }
+    sectionTypes.forEach(sectionType => {
+      lyrics = !lyrics && sections[sectionType] && sections[sectionType].lyrics ? sections[sectionType].lyrics : lyrics
+      chords = !chords && sections[sectionType] && sections[sectionType].chords ? sections[sectionType].chords : chords
+      info = !info && sections[sectionType] && sections[sectionType].info ? sections[sectionType].info : info
+      sections[sectionType] = sections[sectionType] ? sections[sectionType] : {lyrics, chords, info, count: 0}
+      const sectionIndex = sections[sectionType].count
+      sections[sectionType].count++
+      structure = structure.concat({ sectionType, lyrics, chords, sectionIndex, info })
+    })
+    return { title, author, structure, sections }
+  }, {title: rawSections[0].split(' - ')[0], author: rawSections[0].split(' - ')[1], structure: [], sections: {}}))
 })
--- test.js
+++ test.js
@@ -8,34 +8,52 @@ const loadSongsheet = (file) => fs.readFile(file, 'utf8').then(songsheet)
 test('songsheet', function (t) {
   t.test('parse sleeping-on-the-road', t => {
     loadSongsheet('./sleeping-on-the-road.txt')
-      .then(({title, author, structure}) => {
+      .then(({title, author, structure, sections: {verse, chorus}}) => {
         t.equal(title, 'SLEEPING ON THE ROAD')
         t.equal(author, 'WILLIE COTTON')
+        t.equal(verse.count, 4)
+        t.equal(chorus.count, 4)
+        t.deepEqual(structure.map(s => !!s.chords), [ true, true, true, true, true, true, true, true, true, false, true ])
+        t.deepEqual(structure.map(s => !!s.lyrics), [ true, true, true, true, true, true, true, true, true, false, true ])
+        t.deepEqual(structure.map(s => !!s.info), [ false, false, false, false, false, false, false, false, false, false, false ])
         t.deepEqual(structure.map(s => s.sectionType), ['verse', 'chorus', 'verse', 'chorus', 'verse', 'chorus', 'bridge', 'verse', 'chorus', 'instrumental', 'bridge'])
       })
       .then(t.end)
+      .catch(console.log)
   })
 
   t.test('parse riot-on-a-screen', t => {
     loadSongsheet('./riot-on-a-screen.txt')
-      .then(({title, author, structure, otherSections}) => {
+      .then(({title, author, structure, sections: {verse, chorus, fill}}) => {
         t.equal(title, 'RIOT ON A SCREEN')
         t.equal(author, 'WILLIE COTTON')
-        t.equal(otherSections.fill.info, ' D G D A D')
+        t.equal(fill.info, ' D G D A D')
+        t.equal(verse.count, 2)
+        t.equal(chorus.count, 5)
+        t.deepEqual(structure.map(s => !!s.chords), [ true, true, false, true, true, true, false, true, true, false ])
+        t.deepEqual(structure.map(s => !!s.lyrics), [ true, true, false, true, true, true, false, true, true, false ])
+        t.deepEqual(structure.map(s => !!s.info), [ false, false, true, false, false, false, false, false, false, true ])
         t.deepEqual(structure.map(s => s.sectionType), ['verse', 'chorus', 'fill', 'verse', 'chorus', 'chorus', 'instrumental', 'chorus', 'chorus', 'fill'])
       })
       .then(t.end)
+      .catch(console.log)
   })
 
   t.test('parse spent-some-time-in-buffalo', t => {
     loadSongsheet('./spent-some-time-in-buffalo.txt')
-      .then(({title, author, structure, otherSections: {prechorus: {chords, lyrics}}}) => {
+      .then(({title, author, structure, sections: {verse, chorus, prechorus}}) => {
         t.equal(title, 'SPENT SOME TIME IN BUFFALO')
         t.equal(author, 'WILLIE COTTON')
+        t.equal(verse.count, 3)
+        t.equal(chorus.count, 3)
+        t.deepEqual(structure.map(s => !!s.chords), [ true, true, true, true, true, true, true, false, true, true ])
+        t.deepEqual(structure.map(s => !!s.lyrics), [ true, true, true, true, true, true, true, false, true, true ])
+        t.deepEqual(structure.map(s => !!s.info), [ false, false, false, false, false, false, false, false, false, false ])
         t.deepEqual(structure.map(s => s.sectionType), ['verse', 'verse', 'prechorus', 'chorus', 'verse', 'prechorus', 'chorus', 'instrumental', 'prechorus', 'chorus'])
-        t.deepEqual(chords, ['D'])
-        t.deepEqual(lyrics, [' Spent some time in Buffalo', ' Dug my life out of the snow', ' And when those lake winds start to blow', ' I\'ve had my fill it\'s time to go'])
+        t.deepEqual(prechorus.chords, ['D'])
+        t.deepEqual(prechorus.lyrics, [' Spent some time in Buffalo', ' Dug my life out of the snow', ' And when those lake winds start to blow', ' I\'ve had my fill it\'s time to go'])
       })
       .then(t.end)
+      .catch(console.log)
   })
 })
